7103e2732d136016841d2df9b6724cd85dfa393c
KubiK010101 <kubik.nazar13@gmail.com>
2022-10-31 18:25:23 +0200

    locale update

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,7 @@
 
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import en from "./locales/en/translation.json";
+import en from "./localization/en.json";
 
 i18n.use(initReactI18next)
 	// init i18next
